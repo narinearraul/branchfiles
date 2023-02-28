dcfb56ce9f065c5a3af9c692c200042aa5f2fd2f
ncholak <narinearraul@gmail.com>
2023-02-27 22:21:14 -0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,43 @@
 plugins {
-    id("java")
-    id("org.jetbrains.intellij") version "1.10.1"
+  id("java")
+  id("org.jetbrains.intellij") version "1.10.1"
 }
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.1.4")
-    type.set("IC") // Target IDE Platform
+  version.set("2022.1.4")
+  type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
+  }
 
-    patchPluginXml {
-        sinceBuild.set("221")
-        untilBuild.set("231.*")
-    }
+  patchPluginXml {
+    sinceBuild.set("221")
+    untilBuild.set("231.*")
+  }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+  publishPlugin {
+    token.set(System.getenv("PUBLISH_TOKEN"))
+  }
 }
